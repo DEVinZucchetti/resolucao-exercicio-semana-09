7422861f166e4761f9941fb9480240897da3b1bb
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2023-08-04 21:36:19 -0300

    feat: componente CardMedicamento

--- exercicio-semana-09/src/App.vue
+++ exercicio-semana-09/src/App.vue
@@ -1,27 +1,38 @@
 <template>
   <Header />
   <FormularioNovoMedicamento @cadastrar="AdicionarMedicamento" />
+  <div class="container">
+    <CardMedicamento 
+      @favoritar="FavoritarMedicamento" 
+      nome="Dipirona" 
+      laboratorio="Clamed" 
+      preco="5,00" 
+      id="1" 
+    />
+  </div>
 </template>
 
 <script>
 import { v4 as uuidv4 } from 'uuid';
 import Header from "./components/Header.vue"
 import FormularioNovoMedicamento from "./components/FormularioNovoMedicamento/index.vue"
+import CardMedicamento from "./components/CardMedicamento/index.vue"
 
 export default {
   components: {
     Header,
-    FormularioNovoMedicamento
+    FormularioNovoMedicamento,
+    CardMedicamento
   },
-  data(){
+  data() {
     return {
       listaMedicamentos: []
     }
   },
   methods: {
-    AdicionarMedicamento(nome, laboratorio, preco){
+    AdicionarMedicamento(nome, laboratorio, preco) {
 
-      if(nome == "" || laboratorio == "" || preco == 0){
+      if (nome == "" || laboratorio == "" || preco == 0) {
         alert("Preencha todos os dados")
         return
       }
@@ -35,11 +46,19 @@ export default {
       }
 
       this.listaMedicamentos.push(novoMedicamento)
+    },
+    FavoritarMedicamento(id) {
+      debugger
+      alert(id)
     }
   }
 }
 </script>
 
 <style scoped>
-
+.container {
+  display: flex;
+  widows: 100vw;
+  padding: 1em;
+}
 </style>
